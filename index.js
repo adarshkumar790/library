ae4df03a0151a9198c354c4d88ab0203585eba92
adarshkumar790 <126936165+adarshkumar790@users.noreply.github.com>
2024-09-10 08:26:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: [`${window.location.origin}`],
     credentials: true
 }));
 app.use(cookieParser());
